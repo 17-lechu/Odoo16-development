2614efcddd5ede941381a425ed26ca46a47e9e25
lekshmi <lakshmi@akiraplc.com>
2024-05-02 22:32:10 +0530

    files added

---
+++ inventory_custom/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Inventory Customisation',
+    'version': '1.6',
+    'category': 'Inventory',
+    "sequence": 1,
+    'summary': 'Inventory Management',
+    'complexity': 'User Friendly',
+    'description': """
+            This module provide .....
+    """,
+    'author': 'Lekshmi G S',
+    'website': '',
+    'depends': ['base', 'stock'],
+    'data': [
+    ],
+
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
